07be1210770c1fa387c912c31229f20113ae6fb0
legend23 <yu@vtechnology.ru>
2024-01-26 08:55:17 +0500

    add new settings

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from settings import db_login, db_password
+from settings import db_login, db_password, db_host, db_port, db_name
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{db_login}:{db_password}@192.168.1.180:5432/postgres"
+app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{db_login}:{db_password}@{db_host}:{db_port}/{db_name}"
 db = SQLAlchemy(app)
 
 
@@ -11,9 +11,9 @@ class Request(db.Model):
     __tablename__ = "Request"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     cash_amount = db.Column(db.Float, nullable=False)
-    card_number = db.Column(db.String(50), db.ForeignKey('Requisite.card_number'), nullable=False) # tODO что-то тут не так
+    card_number = db.Column(db.String(50), db.ForeignKey('Requisite.card_number'), nullable=False)
     status = db.Column(db.String(16), nullable=False)
-    requests = db.relationship("Requisite", backref="Request") # tODO что-то тут не так
+    requests = db.relationship("Requisite", backref="Request")
 
 class Requisite(db.Model):
     __tablename__ = "Requisite"
@@ -27,6 +27,10 @@ class Requisite(db.Model):
 def hello():
     return "Hello, World!"
 
+@app.route("/create")
+def create():
+    db.create_all()
+    return "ok"
 
 if __name__ == "__main__":
     app.run()
--- settings.py
+++ settings.py
@@ -4,3 +4,6 @@ import os
 load_dotenv()
 db_login = os.getenv("DB_LOGIN")
 db_password = os.getenv("DB_PASSWORD")
+db_host = os.getenv("DB_HOST")
+db_port = os.getenv("DB_PORT")
+db_name = os.getenv("DB_NAME")
